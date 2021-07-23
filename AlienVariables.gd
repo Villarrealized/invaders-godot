63051f6da4181bc5001554edc892c7bee4e13150
Nicholas Villarreal <nvillarreal@protonmail.com>
2021-07-23 09:28:28 -0600

    remove unnecessary code

--- AlienVariables.gd
+++ AlienVariables.gd
@@ -13,6 +13,5 @@ var speed = initial_speed
 
 var initial_laser_speed = Settings.fps * 2
 var laser_speed = initial_laser_speed
-var laser_x_speed = Settings.fps * 2
 
 var point_value = 40
--- HeroLaser.gd
+++ HeroLaser.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 var speed = 4 * Settings.fps
-var x_speed = 2 * Settings.fps
 
 onready var size = $AnimatedSprite.frames.get_frame('default', 0).get_size()
 onready var screen_size = get_viewport_rect().size
@@ -10,10 +9,7 @@ func _ready():
   add_to_group('hero_weapons')
 
 func _process(delta):
-	# make the laser go back and forth on the x-axis
-	# giving a slight blurry effect that improves visuals
-	x_speed = -x_speed
-	var velocity = Vector2(x_speed, -speed)
+	var velocity = Vector2(0, -speed)
 	position += velocity * delta
 
 	if (position.y + size.y / 2) < 0:
